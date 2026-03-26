4ef6b81e82a0ca9f19a8baedd88ca058878047b9
jwmik123 <contact@joelmik.nl>
2026-03-26 15:53:10 +0100

    tiktok policy on embed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'accelerometer=*, gyroscope=*, magnetometer=*',
+          },
+        ],
+      },
+    ]
+  },
+}
 
 export default withNextIntl(nextConfig)
